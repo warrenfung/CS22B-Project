fa2ec175c2c4c6446b3cd3edb909ae51bc87c37b
warrenfung <104642555+warrenfung@users.noreply.github.com>
2023-04-30 23:46:00 -0700

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,22 @@
+import csv
+import datetime
+import time
+
+from win10toast import ToastNotifier
+
+def check_homework_assignments():
+    with open('Project-SJSU-Courses.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        now = datetime.datetime.now()
+        assignments_due = []
+        for row in reader:
+            due_date = datetime.datetime.strptime(row[1], '%Y-%m-%d')
+            if (due_date - now).days == 0:
+                assignments_due.append(row[0])
+        if assignments_due:
+            assignment_names = ', '.join(assignments_due)
+            toaster = ToastNotifier()
+            toaster.show_toast('Upcoming homework assignment', f'{assignment_names} due today', duration=10)
+
+check_homework_assignments()
